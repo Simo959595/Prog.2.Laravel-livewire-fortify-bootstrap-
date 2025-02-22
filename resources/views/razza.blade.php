1e975122b9370bf8fa58dce5114eac17a10176af
Simone Arena <simonearena10@gmail.com>
2025-02-22 12:23:44 +0100

    creazione progetto laravel con installazione di livewire, bootstrap e fortify - testing del funzionamento di livewire con reindirizzamento da una pagina blade collegata in modo standard dalla rotta nel file web.php

--- resources/views/razza.blade.php
+++ resources/views/razza.blade.php
@@ -1,7 +1,19 @@
+{{-- inizio layout --}}
 <x-layout>
-    <div class="row justify-content-center mt-5">
-        <div class="col-12 col-md-6">
-            <livewire:razza/>
-        </div>
+
+{{-- navbar --}}
+<x-navbar />
+
+{{-- inizio body --}}
+<div class="row justify-content-center mt-5">
+    <div class="col-12 col-md-6">
+        <livewire:razza/>
     </div>
+</div>
+{{-- fine body --}}
+
+{{-- footer --}}
+<x-footer />   
+
+{{-- fine layout --}}
 </x-layout>
